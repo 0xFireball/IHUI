506d1b34ee1274ed3a36825f7c289a0f2d947aac
0xFireball <0xFireball@outlook.com>
2017-08-10 21:08:19 -0700

    store ihui theme in docs

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,12 @@
 
+let light = 'ihui-light'
+let dark = 'ihui-dark'
+
+let theme = window.localStorage.getItem('theme')
+window.document.body.classList.add(theme || light)
+
 function toggleTheme() {
-    window.document.body.classList.toggle('ihui-dark')
-    window.document.body.classList.toggle('ihui-light')
+    window.document.body.classList.toggle(dark)
+    window.document.body.classList.toggle(light)
+    window.localStorage.setItem('theme', window.document.body.classList.contains(light) ? light : dark)
 }
